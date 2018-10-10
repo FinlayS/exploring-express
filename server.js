7a10d3a1a55b6e6a3217a425ec8fb874a11bd827
FinlayS <fsgm01@gmail.com>
2018-10-10 20:48:33 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express!</h1>')
+
+    res.send({
+        name: 'Finlay',
+        likes: [
+            'Whiskey',
+            'cheese'
+        ]
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page!')
+});
+
+
+// bad  - send back json with errorMessage
+
+app.get('/bad', (req, res) =>{
+    res.send({
+        statusCode: 400,
+        errorMessage: 'Bad Request'
+    })
+})
+
+app.listen(3000);
