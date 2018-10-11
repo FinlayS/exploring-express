d28e07a09ee67bb02deba999bf7432da0e372b5e
FinlayS <fsgm01@gmail.com>
2018-10-11 19:24:52 +0100

    S5 L43 Rendering templeates with data

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     //res.send('<h1>Hello Express!</h1>')
 
-    res.send({
-        name: 'Finlay',
-        likes: [
-            'Whiskey',
-            'cheese'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the home page of this project',
+        currentYear: new Date().getFullYear()
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page!')
+    res.render('about.hbs', {
+        pageTitle: 'About Pages',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 
